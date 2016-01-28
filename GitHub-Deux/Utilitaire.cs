a31381846f58d5019a2ff2ce459733e175dbf739
PhilSim22 <philsim66@gmail.com>
2016-01-28 14:53:48 -0500

    CreateTable
    Ajouté la fonction CreateTable

--- GitHub-Deux/Utilitaire.cs
+++ GitHub-Deux/Utilitaire.cs
@@ -9,5 +9,15 @@ namespace GitHub_Deux
     static class Utilitaire
     {
         static Random randomGenerator = new Random();
+
+        public static int CreateRandomTable(int lenght, int min, int max)
+        {
+            int[] Table = new int[lenght];
+            for(int i = 0; i < lenght; ++i)
+            {
+                Table[i] = randomGenerator.Next(min, max + 1);
+            }
+            return Table;
+        }
     }
 }
