0902fc118e58de6ef3bd4b19a7497fc279ce9dcd
PhilSim22 <philsim66@gmail.com>
2016-01-28 14:52:46 -0500

    WriteTable
    Ajouté la fonction WriteTable

--- GitHub-Deux/Utilitaire.cs
+++ GitHub-Deux/Utilitaire.cs
@@ -9,5 +9,13 @@ namespace GitHub_Deux
     static class Utilitaire
     {
         static Random randomGenerator = new Random();
+
+		public static void WriteTable(int [] table)
+		{
+			for (int i = 0; i < table.Length; ++i) 
+			{
+				Console.WriteLine (table [i]);
+			}
+		}
     }
 }
