f753a97554650ab48c11452ad4d0fc9b9ef6ec00
PhilSim22 <philsim66@gmail.com>
2016-01-28 14:41:59 -0500

    Création
    Création de la classe staticUtilitaire et d'un Random static.

---
+++ GitHub-Deux/Utilitaire.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Deux
+{
+    static class Utilitaire
+    {
+        static Random randomGenerator = new Random();
+    }
+}
